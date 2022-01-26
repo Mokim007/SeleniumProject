3955453486142b6fb924cc07002ebe9b446e1bcf
Mokim007 <ansarimdmokim2019@gmail.com>
2022-01-26 19:29:09 +0530

    element

--- src/test/java/ElementFindExample.java
+++ src/test/java/ElementFindExample.java
@@ -1,5 +1,8 @@
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ElementFindExample {
@@ -19,6 +22,16 @@ public class ElementFindExample {
 		//String text=driver.findElement(By.xpath("/html/body/p")).getText();
 		String text=driver.findElement(By.tagName("p")).getText();
 		System.out.println(text);
+		List<WebElement> listHyperLink=driver.findElements(By.tagName("a"));
+		System.out.println("Count of Hyper link ->"+listHyperLink.size());
+		List<WebElement> listImg=driver.findElements(By.tagName("img"));
+		System.out.println("Count of Image ->"+listImg.size());
+		List<WebElement> listLi=driver.findElements(By.tagName("li"));
+		System.out.println("Count of Li ->"+listLi.size());
+		List<WebElement> listScript=driver.findElements(By.tagName("script"));
+		System.out.println("Count of Li ->"+listScript.size());
+		
+		
 	}
 	//html/body/p
 }
